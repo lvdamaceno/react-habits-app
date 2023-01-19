8233904fbcee589cf59bf22d411c775194d1acbf
lvdamaceno <lvdamaceno@gmail.com>
2023-01-18 21:54:18 -0300

    Build app layout

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,7 +1,17 @@
-function App() {
+import './styles/globals.css'
+
+// import {Habit} from "./components/Habit"
+
+import { Header } from './components/Header'
+import { SummaryTable } from './components/SummaryTable'
+
+export function App() {
   return (
-    <h1>Hello World</h1>
+    <div className="w-screen h-screen flex justify-center items-center">
+      <div className='w-full max-w-5xl px-6 flex-col gap-16'>
+        <Header />
+        <SummaryTable />
+      </div>
+    </div>
   )
 }
-
-export default App
